52c83108e92e11fe6223305aacc6e756ba85b550
Seth Shoemaker <Shoemakerseth2003@gmail.com>
2023-05-22 16:16:28 -0400

    Added HttpClient for DI Added apiURL environment variable Added HttpClientModule Created api interceptor for rewriting urls for API

--- src/Reflective.Web/src/app/app.module.ts
+++ src/Reflective.Web/src/app/app.module.ts
@@ -4,6 +4,8 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './components/header/header.component';
+import { HTTP_INTERCEPTORS, HttpClientModule } from '@angular/common/http';
+import { ApiInterceptor } from './interceptors/api/api.interceptor';
 
 @NgModule({
   declarations: [
@@ -12,9 +14,12 @@ import { HeaderComponent } from './components/header/header.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    HttpClientModule
   ],
-  providers: [],
+  providers: [
+    { provide: HTTP_INTERCEPTORS, useClass: ApiInterceptor, multi: true },
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
